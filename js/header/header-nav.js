29b2988e3b2c466d5f236845f87ab743a1eee5b8
dxrvcw <oleksandruhnotk5@gmail.com>
2024-03-16 16:50:19 +0200

    Added header-hero section

--- js/header/header-nav.js
+++ js/header/header-nav.js
@@ -3,12 +3,15 @@ const burgerButtonContainer = document.querySelector(
 );
 const burgerButton = document.querySelector(".header-nav__burger-btn");
 const burgerWindow = document.querySelector(".header-nav__burger-window");
+const body = document.querySelector("body");
 
 burgerButtonContainer.addEventListener("click", () => {
 	burgerButton.classList.toggle("active");
 	if (burgerButton.classList.contains("active")) {
 		burgerWindow.style.left = 0 + "px";
+		body.style.overflow = "hidden";
 	} else {
 		burgerWindow.style.left = 100 + "%";
+		body.style.overflow = "auto";
 	}
 });
